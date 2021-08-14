ec5ad5c1362410b42f57b1925decc3f9dc690022
Connie Leung <cleung334@gmail.com>
2021-08-15 00:16:38 +0800

    chore(deps): install dependencies
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { validationSchema } from './envSchema'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import helmet = require('helmet')
+import * as compression from 'compression'
+import * as morgan from 'morgan'
+import * as cors from 'cors'
+import * as express from 'express'
+import { ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+  app.use(compression())
+  app.use(cors())
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: false }))
+  app.use(morgan('dev'))
+  app.use(helmet())
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  )
+
+  const configService = app.get(ConfigService)
+  const version = configService.get<string>('API_VERSION', '1.0')
+
+  const config = new DocumentBuilder()
+    .setTitle('Swagger Task example')
+    .setDescription('The task API description')
+    .setVersion(version)
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
+  const port = configService.get<number>('PORT', 0)
+  await app.listen(port)
 }
-bootstrap();
+bootstrap()
+  .then(() => console.log('Application started successfully'))
+  .catch(console.error)
